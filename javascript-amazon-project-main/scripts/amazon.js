1472057fc082c81026a9b7c5c513dc90f8f6f33e
Tejas Borate <tejas.borate@waiin.com>
2025-03-01 11:15:12 +0530

    generate html for products

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,4 @@
-const products = [
+let products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -470,3 +470,59 @@ const products = [
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
 ];
+
+let html = "";
+products.forEach((product) => {
+  html += `
+            <div class="product-container">
+                <div class="product-image-container">
+                <img class="product-image" src="${product.image}">
+                </div>
+
+                <div class="product-name limit-text-to-2-lines">
+                ${product.name}
+                </div>
+
+                <div class="product-rating-container">
+                <img class="product-rating-stars" src="images/ratings/rating-${
+                  product.rating.stars * 10
+                }.png" >
+                <div class="product-rating-count link-primary">
+                    ${product.rating.count}
+                </div>
+                </div>
+
+                <div class="product-price">
+                $${product.priceCents / 100}
+                </div>
+
+                <div class="product-quantity-container">
+                <select>
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
+                </div>
+
+                <div class="product-spacer"></div>
+
+                <div class="added-to-cart">
+                <img src="images/icons/checkmark.png">
+                Added
+                </div>
+
+                <button class="add-to-cart-button button-primary">
+                Add to Cart
+                </button>
+            </div>
+    `;
+});
+
+document.querySelector(".products-grid").innerHTML = html;
