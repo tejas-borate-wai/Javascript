1a5eb509aa48dada01522fbeebd0bd0500413935
Tejas Borate <tejas.borate@waiin.com>
2025-03-04 11:23:32 +0530

    generate carthtml

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,17 @@
-export let cart = [];
+export let cart = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 10,
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 10,
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    quantity: 50,
+  },
+];
 
 export function addToCart(product_id) {
   let quantity_selector = document.querySelector(
