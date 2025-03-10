9bccdf752a87904f5ec9ef46920144145c1f51a7
Tejas Borate <tejas.borate@waiin.com>
2025-03-10 19:25:54 +0530

    add backend practice file , where i learn  sending response

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,6 +2,7 @@ import { checkoutOrderSummery } from "./checkout/orderSummery.js";
 import { paymentSummery } from "./checkout/paymentSummery.js";
 
 // import "../data/cart-oop.js";
-import "../data/cart-class.js";
+// import "../data/cart-class.js";
+import "../data/backend-practice.js";
 checkoutOrderSummery();
 paymentSummery();
