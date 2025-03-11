bfa56ad8eaebc75db23b87e35123843078f614d8
Tejas Borate <tejas.borate@waiin.com>
2025-03-11 12:11:25 +0530

    learn sample fetch

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -6,3 +6,31 @@ import { paymentSummery } from "./checkout/paymentSummery.js";
 import "../data/backend-practice.js";
 checkoutOrderSummery();
 paymentSummery();
+
+// function first(a) {
+//   console.log("3");
+//   a();
+// }
+
+// console.log("1");
+// new Promise((resolve) => {
+//   console.log("2");
+//   first((resolve) => {
+//     resolve;
+//   });
+// }).then(() => {
+//   console.log(".then called");
+//   console.log("4");
+// });
+
+function loadProducts() {
+  fetch(" https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productData) => {
+      console.log(productData);
+    });
+}
+
+loadProducts();
